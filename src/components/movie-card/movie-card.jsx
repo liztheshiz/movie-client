cdccd6b9070920e796b9a14dcfff775da44f29d2
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-14 12:08:52 -0400

    imported props to MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export class MovieCard extends React.Component {
     render() {
-        return <div className="movie-card">some title</div>;
+        const { movie } = this.props;
+        return <div className="movie-card">{movie.Title}</div>;
     }
 }
