1587552290f32c9c81ca7d408c8d2a632e557411
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-14 12:15:25 -0400

    adjusted spacing in render funcitons

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -15,6 +15,7 @@ export class MainView extends React.Component {
 
     render() {
         const { movies } = this.state;
+
         if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
         return (
             <div className="main-view">
