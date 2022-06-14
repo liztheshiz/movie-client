d59ddce1076f1cfa47416e1ba5638a31b3b61652
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-14 17:15:04 -0400

    added click functionality to movie list

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
     constructor() {
@@ -9,17 +10,27 @@ export class MainView extends React.Component {
                 { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...' },
                 { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...' },
                 { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
-            ]
-        }
+            ],
+            selectedMovie: null
+        };
+    }
+
+    setSelectedMovie(newSelectedMovie) {
+        this.setState({
+            selectedMovie: newSelectedMovie
+        });
     }
 
     render() {
-        const { movies } = this.state;
+        const { movies, selectedMovie } = this.state;
 
+        if (selectedMovie) return <MovieView movie={selectedMovie} />;
         if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
         return (
             <div className="main-view">
-                {movies.map(movie => <MovieCard key={movie._id} movie={movie} />)}
+                {movies.map(movie =>
+                    <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+                )}
             </div>
         );
     }
