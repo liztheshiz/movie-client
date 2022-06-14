7fa5d8db12ca43620ceb87ac5d0bd56a0feced7c
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-14 17:51:28 -0400

    parcel build

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -25338,19 +25338,24 @@ class MainView extends _reactDefault.default.Component {
                 {
                     _id: 1,
                     Title: 'Inception',
-                    Description: 'desc1...',
-                    ImagePath: '...'
+                    Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
+                    Genre: 'Thriller',
+                    Director: 'Christopher Nolan',
+                    ImagePath: '../../img/inception.jpeg'
                 },
                 {
                     _id: 2,
                     Title: 'The Shawshank Redemption',
-                    Description: 'desc2...',
+                    Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+                    Genre: 'Drama',
+                    Director: 'Frank Darabont',
                     ImagePath: '...'
                 },
                 {
                     _id: 3,
                     Title: 'Gladiator',
-                    Description: 'desc3...',
+                    Description: 'A former Roman General sets out to exact vengeance against the corrupt emporer who murdered his family and sent him into slavery.',
+                    Genre: 'Drama',
                     ImagePath: '...'
                 }
             ],
