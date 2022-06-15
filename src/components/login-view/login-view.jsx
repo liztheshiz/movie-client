bce980205b1083e7b4a84d68edc81e323db719e6
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-15 15:44:44 -0400

    initialized LoginView component (no validation logic yet)

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,28 @@
-function LoginView(props) {
+import React, { useState } from 'react';
+
+export function LoginView(props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // prevents page from refreshing when clicking submit button
+        console.log(username, password);
+        /* Send a request to the server for authentication */
+        /* then call props.onLoggedIn(username) */
+        // props.onLoggedIn(username);
+    };
+
     return (
-        // login-obj
+        <form>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+        </form>
     );
 }
