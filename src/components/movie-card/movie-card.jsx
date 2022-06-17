fdc3ac95e92d90cedca930bbeb104f66fe07b8eb
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-17 13:24:35 -0400

    imported card and button into moviecard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 import './movie-card.scss';
 
