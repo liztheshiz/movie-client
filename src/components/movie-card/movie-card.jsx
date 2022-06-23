8cdd8b571f6ea7932d078b677df31c9884e7393a
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-23 16:05:39 -0400

    Revert "added routing functionality to main view"
    This reverts commit 591c417e95a22eddc6bc2f6eb786d0b30cbb5d27.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,23 +3,19 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-import { Link } from "react-router-dom";
-
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
     render() {
-        const { movie } = this.props;
+        const { movie, onMovieClick } = this.props;
 
         return (
             <Card className="movie-card my-3 border-dark border-3" /*style={{ width: '18rem' }}*/>
-                <Card.Img variant="top" src={`../../img/placeholder.png`}/*{movie.ImagePath}*/ />
+                <Card.Img variant="top" src="../../img/placeholder.png"/*{movie.ImagePath}*/ />
                 <Card.Body>
                     <Card.Title>{movie.Title}</Card.Title>
                     <Card.Text>{movie.Description}</Card.Text>
-                    <Link to={`/movies/${movie._id}`}>
-                        <Button variant="outline-dark">View details</Button>
-                    </Link>
+                    <Button variant="outline-dark" onClick={() => { onMovieClick(movie); }}>View details</Button>
                 </Card.Body>
             </Card>
         );
@@ -41,5 +37,6 @@ MovieCard.propTypes = {
             Death: PropTypes.string
         }),
         ImagePath: PropTypes.string.isRequired
-    }).isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 }
