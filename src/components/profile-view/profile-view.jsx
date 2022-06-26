6206ec5b4a6a7e34d27c111a497caaab20843a6d
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-26 16:11:24 -0400

    removed button import from profileview
    button not used

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 
 export class ProfileView extends React.Component {
     render() {
