c775c60ebcef907a8fa4ce5e1dd8fd56c56578db
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-07-05 11:25:09 -0400

    replaced combined reducers with redux combineReducers func

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -22,12 +22,9 @@ function movies(state = [], action) {
     }
 }
 
-function moviesApp(state = {}, action) {
-    return {
-        visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-        movies: movies(state.movies, action)
-    }
-}
+const moviesApp = combineReducers({
+    visibilityFilter, movies
+});
 
 
 export default moviesApp;
