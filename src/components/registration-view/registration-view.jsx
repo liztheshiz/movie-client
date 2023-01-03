ab4ada1b270ccf7199012cece43c673b5158b2b2
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2023-01-03 09:54:53 -0700

    updated sever addresses w new instance dns

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -88,7 +88,7 @@ export function RegistrationView(props) {
                 Email: email,
                 Birthday: birthday
             };
-            axios.post('http://ec2-35-172-250-209.compute-1.amazonaws.com:8081/users', request).then(res => {
+            axios.post('http://ec2-44-204-142-22.compute-1.amazonaws.com:8081/users', request).then(res => {
                 alert('User successfully registered! Redirecting to login...');
                 window.open('/', '_self');
 
