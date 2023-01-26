7e48707325a0a4ac3140c326d5f668158fdf187e
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2023-01-26 16:09:24 -0500

    added showimage functionality to object cards

--- src/object-card/object-card.jsx
+++ src/object-card/object-card.jsx
@@ -4,23 +4,40 @@ import axios from 'axios';
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-import { Link } from 'react-router-dom';
 
 //import './object-card.scss';
 
 export class ObjectCard extends React.Component {
+    // CUSTOM METHODS
+
+    getObject() {
+
+        this.setState({
+            showImage: true
+        });
+        console.log('button clicked!');
+    }
+
+    // LIFECYCLE METHODS
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showImage: false
+        };
+    }
+
     render() {
         const { object } = this.props;
+        const { showImage } = this.state;
 
         return (
             <Card className="object-card my-3">
                 <Card.Body>
                     <Card.Title className="title fs-4">{object.Key}</Card.Title>
-                    <Button className="button" variant="outline-dark" onClick={() => console.log('great job! you clicked the button!')}>Click me</Button>
+                    <Button className="button" variant="outline-dark" onClick={() => this.getObject()}>View file</Button>
                 </Card.Body>
+                {showImage && <Card.Img variant="top" crossOrigin="anonymous" src={'../img/favicon.ico'} />}
             </Card >
         );
     }
