b64695073f6718bbc8f30f04eb9debcdcac1a20f
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2023-02-08 10:27:09 -0500

    pass showImage func to objectCard

--- src/components/objects-list/objects-list.jsx
+++ src/components/objects-list/objects-list.jsx
@@ -9,11 +9,15 @@ import { ObjectCard } from '../object-card/object-card';
 export function ObjectsList(props) {
     const { objects } = props;
 
+    const showImage = () => {
+        console.log('click function was called!');
+    }
+
     return (
         <Row className="justify-content-center">
             {objects.map(m =>
                 <Col xs={11} sm={6} lg={4} xl={3}>
-                    <ObjectCard object={m} />
+                    <ObjectCard object={m} showImage={showImage} />
                 </Col>
             )}
         </Row>
